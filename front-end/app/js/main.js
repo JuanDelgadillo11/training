e3369230e2888899c77cccfd9222f4c8b5ee75b6
Juan Delgadillo <jdelgadillo111@gmail.com>
2015-06-18 20:06:42 -0700

    task3 first commit

--- front-end/app/js/main.js
+++ front-end/app/js/main.js
@@ -1,3 +1,9 @@
+/*
+* Author: Juan Delgadillo
+* Task1: Create function to sum, max,min and avg
+* */
+
+
 var calculateAll = function(){
     console.log('SUMA: ', sum1(arguments));
 	document.getElementById("p1").innerHTML="Result:"+sum1(arguments);
@@ -101,3 +107,211 @@ var promedio=0;
 	
 	return promedio;
 };
+
+
+
+/*
+* Task 3
+* This class contains functions for sum, Max, min, avg, add, subtract, multiply and divide
+ */
+calculadora1= {
+	memory:0,
+
+	delMemory:function()
+	{
+		this.memory=0;
+		return this.memory;
+	},
+
+	addMemory:function(value)
+	{
+		this.memory+=value;
+	},
+
+	suma : function(arguments, pos)
+	{
+		var result = this.sum(arguments,pos);
+		this.memory+=result;
+		return result;
+	},
+
+	major : function(arguments, pos)
+	{
+		var result = this.max(arguments,pos);
+		this.memory+=result;
+		return result;
+	},
+
+	minor : function(arguments, pos)
+	{
+		var result = this.min(arguments,pos);
+		this.memory+=result;
+		return result;
+	},
+
+	averageCalc : function(arguments, pos)
+	{
+		var result = this.avg(arguments,pos);
+		this.memory+=result;
+		return result;
+	},
+
+	subtraction: function(arguments,pos)
+	{
+		var result = this.substract(arguments,pos);
+		this.memory-=result;
+		return result;
+	},
+
+	multiplication: function(arguments,pos)
+	{
+		var result = this.multiply(arguments,pos);
+		this.memory*=result;
+		return result;
+	},
+
+	division: function(arguments,pos)
+	{
+		var result = this.divide(arguments,pos);
+		this.memory/=result;
+		return result;
+	},
+
+	sum: function(arguments, pos){
+
+		if (!pos)
+			pos = 0;
+
+		if (pos == arguments.length - 1)
+		{
+			//this.memory += arguments[pos];
+			return arguments[pos];
+		}
+		//var result=  arguments[pos] + this.sum(arguments, pos + 1);
+		//this.memory += result;
+		return arguments[pos] + this.sum(arguments, pos + 1)
+		//this.addMemory(result);
+		//return result;
+	},
+	max: function(arguments, pos){
+
+		var result=0;
+		if (!pos)
+			pos = 0;
+
+		if (pos == arguments.length - 1){
+
+			return result = arguments[0];
+			console.log("result"+result);
+
+		}
+		else{
+			if(arguments[0]<arguments[pos+1]){
+
+				var aux=arguments[pos+1];
+				arguments[pos+1]=arguments[0];
+				arguments[0]=aux;
+
+			}
+
+			return this.max(arguments,pos+1)
+		}
+	},//MAX Function
+
+	min: function(arguments, pos){
+
+		var result=0;
+		if (!pos)
+			pos = 0;
+
+		if (pos == arguments.length - 1){
+			//document.getElementById("p3").innerHTML="Result:"+resultado;
+			return result = arguments[0];
+			console.log("result"+result);
+		}
+		else{
+			if(arguments[0]>arguments[pos+1]){
+				//console.log("Entrando al if");
+				var aux=arguments[pos+1];
+				console.log(aux);
+				arguments[pos+1]=arguments[0];
+				console.log(arguments[pos+1]);
+				arguments[0]=aux;
+				console.log(arguments[0]);
+
+			}
+
+			return this.min(arguments,pos+1)
+		}
+	},//End min function
+
+	avg: function(arguments, pos){
+		var result=0;
+		var average=0;
+		if (!pos)
+			pos = 0;
+
+		if (pos == arguments.length - 1)
+			return result = arguments[pos];
+
+		result = arguments[pos] + sum(arguments, pos + 1)
+
+		average = result/parseInt(arguments.length);
+		console.log("average"+average);
+		console.log("size"+parseInt(arguments.length));
+
+		return average;
+	},
+
+	substract: function(arguments, pos){
+
+		if (!pos)
+			pos = 0;
+
+		if (pos == arguments.length - 1)
+		{
+			//this.memory += arguments[pos];
+			return arguments[pos];
+		}
+		//var result=  arguments[pos] + this.sum(arguments, pos + 1);
+		//this.memory += result;
+		return arguments[pos] - this.substract(arguments, pos + 1)
+		//this.addMemory(result);
+		//return result;
+	},
+
+	multiply: function(arguments, pos){
+
+		if (!pos)
+			pos = 0;
+
+		if (pos == arguments.length - 1)
+		{
+			//this.memory += arguments[pos];
+			return arguments[pos];
+		}
+		//var result=  arguments[pos] + this.sum(arguments, pos + 1);
+		//this.memory += result;
+		return arguments[pos] * this.substract(arguments, pos + 1)
+		//this.addMemory(result);
+		//return result;
+	},
+
+	divide: function(arguments, pos){
+
+		if (!pos)
+			pos = 0;
+
+		if (pos == arguments.length - 1)
+		{
+			//this.memory += arguments[pos];
+			return arguments[pos];
+		}
+		//var result=  arguments[pos] + this.sum(arguments, pos + 1);
+		//this.memory += result;
+		return arguments[pos] / this.substract(arguments, pos + 1)
+		//this.addMemory(result);
+		//return result;
+	}
+
+}
